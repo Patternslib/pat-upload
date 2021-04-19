98d7c837297858883d48877dcef4127cfbd10457
Johannes Raggam <thetetet@gmail.com>
2021-04-19 16:48:59 +0200

    maint: Add basic test.

---
+++ src/pat-upload.test.js
@@ -0,0 +1,20 @@
+import "regenerator-runtime/runtime"; // needed for ``await`` support
+import pattern from "./pat-upload";
+import utils from "@patternslib/patternslib/src/core/utils";
+
+describe("pat-upload", () => {
+    beforeEach(() => {
+        document.body.innerHTML = "";
+    });
+
+    it("is initialized correctly", async (done) => {
+        document.body.innerHTML = `<fieldset class="pat-upload" />`;
+
+        pattern.init(document.querySelector(".pat-upload"));
+        await utils.timeout(1);
+
+        expect(document.body.querySelectorAll(".upload-area").length).toBe(1);
+
+        done();
+    });
+});
